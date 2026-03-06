a5cf431c174b2b6a72e1d7b63966317b8c8fcf1a
ArmanGuptaCSE <armangupta.dev@gmail.com>
2026-03-06 22:47:07 +0530

    added python variables practice questions

---
+++ variable.py
@@ -0,0 +1,36 @@
+# VARIABLES PRACTICE
+
+
+# Question 1: Create a variable storing your name
+name = "Arman gupta"
+print(name)
+
+
+# Question 2: Store your age in a variable and print it
+age = 18
+print(age)
+
+
+# Question 3: Store two numbers and print their sum
+a = 15
+b = 10
+print(a + b)
+
+
+# Question 4: Print the data type of a variable
+x = 5.5
+print(type(x))
+
+#Question 5:Create three variables named name, age, and college.Store appropriate values in them and print each variable on a separate line.
+name = "Arman"
+age = 19
+college = "bml munjal university "
+print(name)
+print(age)
+print(college)
+
+#Question 8:Create two variables length and width.Calculate and print the area of a rectangle using these variables.
+length = 33
+width =22
+area_of_rectangle = length*width
+print(area_of_rectangle)
