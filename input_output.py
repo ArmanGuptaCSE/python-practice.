69d3842b05cf31d06a904052c1398482acc01703
ArmanGuptaCSE <armangupta.dev@gmail.com>
2026-03-07 15:22:32 +0530

    Added input and output practice questions

---
+++ input_output.py
@@ -0,0 +1,45 @@
+#INPUT_OUTPUT PRACTICE
+
+#QUESTION 1: Take the user's name as input and print a welcome message.
+name = input("enter your name:")
+print(" welcome:",name)
+
+#QUESTION 2: Take age as input and print
+age = int(input(" enter your age:"))
+print(" your age is", age)
+
+#QUESTION 3: Take two numbers from the user and print their sum.
+number_1 = int(input(" enter your first number:"))
+number_2 = int(input("enter your second number:"))
+sum = number_1 + number_2
+print(" the sum of above numbers is:",sum)
+
+#QUESTION 4: Take name and age from the user and print
+input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("My name is", name, "and I am", age, "years old")
+
+#QUESTION 5: Take a number and print double of that number.
+num = int(input("enter the number:"))
+double = num*num
+print(" the double of the above number is:",double)
+
+#QUESTION 6: take a number and print double of that number.
+length = int(input(" enter the length of rectangle:"))
+width = int(input(" enter the width of the rectangle:"))
+area = length*width
+print("the area is:",area)
+
+
+#QUESTION 7: Take three numbers from the user and print their average.
+num1= int(input(" enter 1st number"))
+num2= int(input(" enter your 2nd number"))
+num3 = int(input(" enter your third number:"))
+average = (num1+num2+num3)/3
+print("the average of the above three numbers is:", average)
+
+#QUESTION 8 : take distance (km) and time (hours) and calculate speed.
+distance = int(input("enter the distance travelled:"))
+time = int(input("enter the time taken to travel the ditance:"))
+speed = distance/time
+print(" the speed is",speed)
